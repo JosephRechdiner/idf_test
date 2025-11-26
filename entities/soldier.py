3be0d09f5b567bd202174be42d57bbd5b948a9cb
JosephRer <jrechdiner2002@gmail.com>
2025-11-26 11:35:03 +0200

    decided to leave using db and models and start oop, added soldier

---
+++ entities/soldier.py
@@ -0,0 +1,10 @@
+class Soldier:
+    def __init__(self, personal_number, first_name, last_name, sex, city, distance):
+        self.personal_number = personal_number
+        self.first_name = first_name
+        self.last_name = last_name
+        self.sex = sex
+        self.city = city
+        self.distance = distance
+        self.has_room = False
+
